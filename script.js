6a5bfea055c04507c1405e1473a83e98800c7586
SimonLippitt <133723073+SimonLippitt@users.noreply.github.com>
2023-10-22 15:05:21 -0400

    initialize repos

---
+++ script.js
@@ -0,0 +1,105 @@
+const quizData = [
+    {
+        question: 'What year was AI first developed?',
+        a: '1997',
+        b: '1956',
+        c: '2019',
+        d: '1974',
+        correct: 'b'
+    }, {
+        question: 'Which area does AI have great potential in?',
+        a: 'Business',
+        b: 'Medicine',
+        c: 'Education',
+        d: 'All of the above',
+        correct: 'd'
+    }, {
+        question: 'Which company developed ChatGPT?',
+        a: 'Microsoft',
+        b: 'OpenAI',
+        c: 'Google',
+        d: 'Oracle',
+        correct: 'b'
+    }, {
+        question: 'Which one is an open source platform for Machine Learning?',
+        a: 'MySQL',
+        b: 'HTML',
+        c: 'TensorFLow',
+        d: 'Pycharm',
+        correct: 'c'
+    }, {
+        question: 'How are language processing models trained?',
+        a: 'RLHF',
+        b: 'Statistical Modeling',
+        c: '1 and 2',
+        d: 'none of the above',
+        correct: 'c'
+    }
+];
+
+
+const quiz = document.getElementById('quiz');
+const answerEls = document.querySelectorAll('.answer');
+const questionEl = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit');
+
+
+let currentQuiz = 0;
+let score = 0;
+
+
+loadQuiz();
+
+function loadQuiz() {
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuiz];
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+}
+
+function getSelected() {
+    let answer = undefined;
+
+    answerEls.forEach(answerEl => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+
+function deselectAnswers() {
+
+    answerEls.forEach(answerEl => {
+        answerEl.checked = false;
+
+    });
+}
+
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected();
+
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `<h2>You answered ${score}/${quizData.length} correctly.</h2> <button onclick="location.reload()">Reload</button>`;
+        }
+    }
+});
